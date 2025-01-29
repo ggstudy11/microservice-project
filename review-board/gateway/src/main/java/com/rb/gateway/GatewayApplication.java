38b9f202cd3390f018c7a36308e3dff86d62b7f8
ggstudy11 <439403849@qq.com>
2025-01-29 15:49:29 +0800

    网关配置

---
+++ review-board/gateway/src/main/java/com/rb/gateway/GatewayApplication.java
@@ -0,0 +1,18 @@
+package com.rb.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ：ggstudy11
+ * @version :
+ * @date ：Created in 1/29/2025 2:49 PM
+ * @description：网关启动类
+ * @modified By：
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
